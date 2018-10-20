5be0d710b66e5922f21826d1e629c114b4becd2e
Unknown <hoejsgaard.tobias@gmail.com>
2018-10-20 06:01:54 +0200

    Worksheet 3 done except for theory, good progress on worksheet 4

--- worksheet4/graphics.js
+++ worksheet4/graphics.js
@@ -1,7 +1,8 @@
 var index = 0;
 var theta = 1.0;
 var modelViewMatrix, ctm;
-var indices, vertices;
+var indices, vertices, pointsArray;
+var subs;
 
 var iso = mat4(1.732050808/2.449489743,0,-1.732050808/2.449489743,0,
 				1/2.449489743,2/2.449489743,1/2.449489743,0,
@@ -30,7 +31,7 @@ var look, translate, scale, ort, pin, projection, temp;
 
 var selected = 1;
 
-var showClassic;
+var showClassic = true;
 
 /**
 * @param {Element} canvas. The canvas element to create a context from.
@@ -41,6 +42,23 @@ function setupWebGL(canvas) {
 }
 
 window.onload = function init(){
+	
+		subs = 0;
+		document.getElementById("inc").addEventListener("mousedown", function() {
+			if(subs<10){
+				subs += 1;
+			}
+			pointsArray = [];
+			tetrahedron(va, vb, vc, vd, subs);
+		});
+		
+		document.getElementById("dec").addEventListener("mousedown", function() {
+			if(subs>0){
+				subs -= 1;
+			}
+			pointsArray = [];
+			tetrahedron(va, vb, vc, vd, subs);
+		});
 	
 		canvas = document.getElementById("webgl");
 	
@@ -55,6 +73,22 @@ window.onload = function init(){
 		
 		program = initShaders(gl, "vertex-shader", "fragment-shader");
 		gl.useProgram(program);
+		
+		var va = vec4(0.0, 0.0, -1.0, 1);
+		var vb = vec4(0.0, 0.942809, 0.333333, 1);
+		var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+		var vd = vec4(0.816497, -0.471405, 0.333333, 1);
+		
+		pointsArray = [];
+		
+		tetrahedron(va, vb, vc, vd, subs);
+		
+		/*
+		pointsArray.push(va);
+		pointsArray.push(vb);
+		pointsArray.push(vc);
+		pointsArray.push(vd);
+		index += 4;*/
 		
 		vertices = [
 			vec3(0.0, 0.0, 0.0),
@@ -77,32 +111,22 @@ window.onload = function init(){
 			0,1,1, 
 			0,0,0];
 		
-		indices = [
-			1, 0, 3,
-			3, 2, 1,
-			2, 3, 7,
-			7, 6, 2,
-			3, 0, 4,
-			4, 7, 3,
-			6, 5, 1,
-			1, 2, 6,
-			4, 5, 6,
-			6, 7, 4,
-			5, 4, 0,
-			0, 1, 5
-		];
-			
+		indices = new Uint8Array(index);
+		for(var i=0; i<indices.length; i++){
+			indices[i] = i;
+		}
+		
 		var vBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 		
 		var vPosition = gl.getAttribLocation(program, "vPosition");
-		gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+		gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(vPosition);
 		
 		var iBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices),gl.STATIC_DRAW);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 		
 		
 		modelViewMatrix = mat4();
@@ -122,81 +146,67 @@ window.onload = function init(){
 		gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(vColor);
 		
-		showClassic = true;
 		ort = ortho(-1.0, 1.0, -1.0, 1.0, 0.01, 1000.0);	
 		pin = perspective(45.0, canvas.width/canvas.height, 0.01, 1000.0);
-		
-		document.getElementById("iso").onclick = function(){
-			showClassic = false;
-			render();
-		}
-		
-		document.getElementById("one").onclick = function(){
-			showClassic = true;
-			render();
-		}
-		
-		/*
-		document.getElementById("two").onclick = function(){
-			selected = 2;
-		}
-		
-		document.getElementById("three").onclick = function(){
-			selected = 3;
-		}*/	
 			
 		render();
 	}
 
-
-	function render(){		
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
-		if(showClassic){
-			look = lookAt(vec3(3.0,0.25,0.25), vec3(0.25,0.25,0.25), vec3(0.0,1.0,0.0));
-			scale = scalem(0.5,0.5,0.5);
-			projection = mult(pin, look);
-			projection = mult(projection, scale);
-			
-			ctm = mat4();
-			ctm = mult(ctm, projection);
-			gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
-			gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_BYTE, 0);
-			
-			temp = projection;
-			projection = mult(projection, translate(0.0, 0.0, 1.5));
-			projection = mult(projection, rotateY(45.0));
-			
-			
-			ctm = mat4();
-			ctm = mult(ctm, projection);
-			gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
-			gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_BYTE, 0);
-			
-			projection = temp;
-			projection = mult(projection, translate(0.0, 0.0, -2.0));
-			projection = mult(projection, rotateY(45.0));
-			projection = mult(projection, rotateZ(45.0));
-			projection = mult(projection, translate(-0.7, 0.2, 0.0));
-			
-			ctm = mat4();
-			ctm = mult(ctm, projection);
-			gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
-			gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_BYTE, 0);
-			
-			
-		} else {
-			look = lookAt(vec3(1.5,1.5,1.5), vec3(0.0,0.0,0.0), vec3(0.0,1.0,0.0));
-			projection = mult(ort, look);
-			
-			ctm = mat4();
-			ctm = mult(ctm, projection);
-			gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
-			gl.drawElements(gl.LINE_STRIP, indices.length, gl.UNSIGNED_BYTE, 0);
+	function triangle(a, b, c){
+			pointsArray.push(c);
+			pointsArray.push(b);
+			pointsArray.push(a);
+			index += 3;
 		}
 		
-	
+	function divideTriangle(a, b, c, count)
+	{
+		if (count > 0) {
+			var ab = normalize(mix(a, b, 0.5), true);
+			var ac = normalize(mix(a, c, 0.5), true);
+			var bc = normalize(mix(b, c, 0.5), true);
+			divideTriangle(a, ab, ac, count - 1);
+			divideTriangle(ab, b, bc, count - 1);
+			divideTriangle(bc, c, ac, count - 1);
+			divideTriangle(ab, bc, ac, count - 1);
+		}
+		else {	
+			triangle(a, b, c);
+		}
+	}
 		
+	function tetrahedron(a, b, c, d, n)
+	{
+		divideTriangle(a, b, c, n);
+		divideTriangle(d, c, b, n);
+		divideTriangle(a, d, b, n);
+		divideTriangle(a, c, d, n);
+	}
+
+	function render(){		
+		
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		
+		gl.enable(gl.DEPTH_TEST);
+		gl.enable(gl.CULL_FACE);
+		gl.cullFace(gl.BACK);
+		
+		var vBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+		
+		var vPosition = gl.getAttribLocation(program, "vPosition");
+		gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(vPosition);
+		
+		look = lookAt(vec3(5.0,0.0,0.0), vec3(0.0,0.0,0.0), vec3(0.0,1.0,0.0));
+		projection = mult(pin, look);
+			
+		ctm = mat4();
+		ctm = mult(ctm, projection);
+		gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
+		gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+			
 		/*
 		ctm = mult(ctm, mvs[2]);
 		gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(ctm));
